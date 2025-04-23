d6a868444587e84d4fff179ea7563490d6f4345c
Janez Rotman <janez.rotman@tum.de>
2025-04-23 17:32:28 +0200

    client: Add shadcn to the project and add a simple button component

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,15 @@
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "/check24-gendev-7/"
+  plugins: [vue(), tailwindcss()],
+  base: "/check24-gendev-7/",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
