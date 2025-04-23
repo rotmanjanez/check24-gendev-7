415c866a2b912746bf8ee66b3f493f5818c9aaed
Janez Rotman <janez.rotman@tum.de>
2025-04-23 21:32:56 +0200

    client: simple app to test new component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import PeronalisationSelector from '@/components/PeronalisationSelector.vue'
+
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
 </script>
 
+
 <template>
-  <Button>Click me</Button>
+  <div class="container mx-auto p-6">
+    <div class="flex justify-between items-center mb-6">
+      <div>
+        <h1 class="text-2xl font-bold">{{ t('app.title') }}</h1>
+        <p class="text-gray-400">
+          {{ t('app.subtitle') }}
+        </p>
+      </div>
+      <PeronalisationSelector />
+    </div>
+  </div>
 </template>
