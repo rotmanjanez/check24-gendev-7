c7dd373c79cbb29afc48b5a70baff1094eac284c
Janez Rotman <janez.rotman@tum.de>
2025-05-08 16:47:08 +0200

    client: add address form component

--- client/src/i18n/locales/de.ts
+++ client/src/i18n/locales/de.ts
@@ -10,5 +10,76 @@ export default {
             dark: "Dunkel",
             system: "System"
         }
-    }
+    },
+    addressForm: {
+        countries: {
+            DE: { name: "Deutschland" },
+            AT: { name: "Österreich" },
+            CH: { name: "Schweiz" },
+        },
+        edit: "Adresse ändern",
+        address: {
+            title: "Adresse",
+            placeholder: "Gib Deine Adresse ein",
+            description: "Bitte gib Deine vollständige Adresse ein, um die besten Internet-Optionen zu finden.",
+        },
+        street: {
+            title: "Straße",
+            placeholder: "Straßenname",
+            description: "Der Name der Straße, in der Du Internet nutzen möchtest",
+            errors: {
+                too_small: "Die Straße muss mindestens {minimum} Zeichen haben",
+                too_big: "Die Straße darf maximal {maximum} Zeichen haben",
+                invalid_type: "Die Straße enthält ungültige Zeichen",
+            }
+        },
+        houseNumber: {
+            title: "Hausnummer",
+            placeholder: "Hausnummer",
+            description: "Die Nummer des Hauses oder Deiner Wohnung",
+            errors: {
+                too_small: "Die Hausnummer muss mindestens {minimum} Zeichen haben",
+                too_big: "Die Hausnummer darf maximal {maximum} Zeichen haben",
+                invalid_type: "Die Hausnummer enthält ungültige Zeichen",
+            }
+        },
+        postalCode: {
+            title: "Postleitzahl",
+            placeholder: "Postleitzahl",
+            description: "Die Postleitzahl Deiner Region",
+            errors: {
+                too_small: "Die Postleitzahl muss mindestens {minimum} Zeichen haben",
+                too_big: "Die Postleitzahl darf maximal {maximum} Zeichen haben",
+                invalid_type: "Die Postleitzahl enthält ungültige Zeichen",
+            }
+        },
+        region: {
+            title: "Region",
+            placeholder: "Region",
+            description: "Die Stadt oder Region, in der Du lebst",
+            errors: {
+                too_small: "Die Stadt muss mindestens {minimum} Zeichen haben",
+                too_big: "Die Stadt darf maximal {maximum} Zeichen haben",
+                invalid_type: "Die Stadt enthält ungültige Zeichen",
+            }
+        },
+        country: {
+            title: "Land",
+            placeholder: "Land",
+            description: "Das Land, in dem Du lebst",
+        },
+        countryCode: {
+            title: "Ländercode",
+            placeholder: "Ländercode",
+            description: "Der Ländercode, in dem Du lebst",
+            errors: {
+                invalid_enum_value: "Der Ländercode ist ungültig",
+                invalid_type: "Der Ländercode ist ungültig",
+            }
+        },
+        submit: "Jetzt vergleichen",
+    },
+    errors: {
+        unknown: "Ein unbekannter Fehler ist aufgetreten.",
+    },
 } as const
--- client/src/i18n/locales/en.ts
+++ client/src/i18n/locales/en.ts
@@ -10,5 +10,76 @@ export default {
             dark: "Dark",
             system: "System"
         }
-    }
+    },
+    addressForm: {
+        countries: {
+            DE: { name: "Germany" },
+            AT: { name: "Austria" },
+            CH: { name: "Switzerland" },
+        },
+        edit: "Edit Adress",
+        address: {
+            title: "Address",
+            placeholder: "Enter your address",
+            description: "Please enter your full address to find the best internet options available.",
+        },
+        street: {
+            title: "Street",
+            placeholder: "Street Name",
+            description: "The name of the street where you want to use the internet",
+            errors: {
+                too_small: "Street name must be at least {minimum} characters",
+                too_big: "Street name cannot exceed {maximum} characters",
+                invalid_type: "Street name contains invalid characters",
+            }
+        },
+        houseNumber: {
+            title: "House Number",
+            placeholder: "House Number",
+            description: "The number of your house or apartment",
+            errors: {
+                too_small: "House number must be at least {minimum} characters",
+                too_big: "House number cannot exceed {maximum} characters",
+                invalid_type: "House number contains invalid characters",
+            }
+        },
+        postalCode: {
+            title: "Postal Code",
+            placeholder: "Postal Code",
+            description: "The postal code of your area",
+            errors: {
+                too_small: "Postal code must be at least {minimum} characters",
+                too_big: "Postal code cannot exceed {maximum} characters",
+                invalid_type: "Postal code contains invalid characters",
+            }
+        },
+        region: {
+            title: "Region",
+            placeholder: "Region",
+            description: "The citiy or region where you live",
+            errors: {
+                too_small: "City name must be at least {minimum} characters",
+                too_big: "City name cannot exceed {maximum} characters",
+                invalid_type: "City name contains invalid characters",
+            }
+        },
+        country: {
+            title: "Country",
+            placeholder: "Country",
+            description: "The country where you live",
+        },
+        countryCode: {
+            title: "Country Code",
+            placeholder: "Country Code",
+            description: "The country code of your area",
+            errors: {
+                invalid_enum_value: "Country code is invalid",
+                invalid_type: "Country code is invalid",
+            }
+        },
+        submit: "Compare Now",
+    },
+    errors: {
+        unknown: "An unknown error occurred.",
+    },
 } as const
