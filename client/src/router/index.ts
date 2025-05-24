f80b33309fbf3c65bfeac861dfe04660e8ad9ac9
Janez Rotman <janez.rotman@tum.de>
2025-05-24 16:41:22 +0200

    client: remove shared product view

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,10 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import ProductSearchView from '@/views/ProductSearchView.vue'
-import SharedProductsView from '@/views/SharedProductsView.vue'
 
 const routes = [
     { path: '/', name: 'products', component: ProductSearchView },
-    { path: '/internetproducts/share', name: 'shared-products', component: SharedProductsView },
 ]
 
 export const router = createRouter({
