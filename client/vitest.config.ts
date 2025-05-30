67f8d452c72124fd4a391fcaa0df532fc5e11813
Janez Rotman <janez.rotman@tum.de>
2025-05-30 17:56:44 +0200

    client: add vitest

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,15 +1,14 @@
-import path from 'node:path'
-import tailwindcss from '@tailwindcss/vite'
-import vueI18n from '@intlify/unplugin-vue-i18n/vite'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { configDefaults } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
+import { configDefaults } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
+import vueI18n from '@intlify/unplugin-vue-i18n/vite';
 
 export default defineConfig({
   plugins: [vue(), tailwindcss(), vueI18n({
     runtimeOnly: false,
     include: [path.resolve(__dirname, './src/i18n/locales/**')],
-    
   })],
   base: "/check24-gendev-7/",
   resolve: {
@@ -20,9 +19,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // Backend server
-        changeOrigin: true, 
-        rewrite: (path) => path.replace(/^\/api/, ''), // Remove '/api' prefix
+        target: 'http://gendev-server:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
@@ -50,4 +49,4 @@ export default defineConfig({
     },
     exclude: [...configDefaults.exclude, 'e2e/**'],
   },
-})
+});
